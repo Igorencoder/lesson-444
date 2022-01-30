a9a64880c680d9ef053d63b1d96724af0e3b07f4
geekbrains <info@geekbrains.ru>
2022-01-30 23:18:37 +0300

    DZ 7

--- src/main/java/ru/lesson7/MainClass.java
+++ src/main/java/ru/lesson7/MainClass.java
@@ -13,7 +13,9 @@ public class MainClass {
          else {
             plate.info();
         }
+
          /**  ---ниже пополение тарелки
+          *
           *
           */
          plate.getFood();
